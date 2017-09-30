0543e359f448d651e013804f226788e60840aaa3
CheCm19 <cesarmiranda@Cesars-MacBook-Pro.local>
2017-09-30 01:50:20 -0700

    - Cleans up app.js - App.js is now exported as a module - Established app execution convention bin/www file, express app may now be starte by executing this file vs using npm start - Add test file for session, and established a 1 to 1 convention for test to controller

--- app.js
+++ app.js
@@ -1,24 +1,19 @@
-// app.js
+// Requires
+var express  		= require('express');
+var mongoose 		= require('mongoose');
+var passport 		= require('passport');
+var flash    		= require('connect-flash');
+var morgan       	= require('morgan');
+var cookieParser 	= require('cookie-parser');
+var bodyParser   	= require('body-parser');
+var session      	= require('express-session');
+var configDB     	= require('./config/database.js');
 
-// set up ======================================================================
+// Create Express app instance
+var app      	 	= express();
 
-var express  = require('express');
-var app      = express();
-var port     = process.env.PORT || 8080;
-var mongoose = require('mongoose');
-var passport = require('passport');
-var flash    = require('connect-flash');
-
-var morgan       = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser   = require('body-parser');
-var session      = require('express-session');
-
-var configDB = require('./config/database.js');
-
-// configuration ===============================================================
+// configurations 
 mongoose.connect(configDB.url); // connect to our database
-
 require('./config/passport')(passport); // pass passport for configuration
 
 // set up our express application
@@ -32,10 +27,8 @@ app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
 
-// routes ======================================================================
+// routes 
 require('./app/controllers/session.js')(app, passport); // load our routes and pass in our app and fully configured passport
 
-// launch ======================================================================
-app.listen(port);
-console.log('The magic happens on port ' + port);
-
+// Export App as a module
+module.export = app;
