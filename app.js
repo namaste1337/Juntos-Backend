ab68e743e8f9386c42ec88a6fdd435d3a0bef664
CheCm19 <che.cm@mail.com>
2017-10-06 06:08:40 -0700

    Sets up the password-reset package middleware

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ var bodyParser   	= require('body-parser');
 var session      	= require('express-session');
 var configDB     	= require('./config/database.js');
 var jsend 			= require('jsend');
+var passwordReset 	= require('password-reset')({
+	uri : "http://localhost:8080/password_reset",
+	from : "support@localhost",
+	host : "localhost", port : 25
+});
 
 // Create Express app instance
 var app      	 	= express();
@@ -22,6 +27,8 @@ app.use(morgan('dev')); // log every request to the console
 app.use(cookieParser()); // read cookies (needed for auth)
 app.use(bodyParser.json()); // Get JSON body information
 app.use(jsend.middleware); //Set up Jsend for standardized responses
+app.use(passwordReset.middleware); // Set password reset middlware
+
 
 // required for passport
 app.use(session({ secret: 'f239hd794298hfd8623b(&F#(HFFHq93rh98wefh23' })); // session secret
