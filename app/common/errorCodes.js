b26803f03a043858693a4dc3fa0e76602f9bb811
CheCm19 <che.cm@mail.com>
2017-10-19 20:12:18 -0700

    Moves the passport middleware to the controllers function to better handles errors. Adds common file errorCodes.js

---
+++ app/common/errorCodes.js
@@ -0,0 +1,7 @@
+// This file will host error codes that will
+// shares accross files.
+module.exports = {
+
+	ERROR_CODE_401: 401
+
+}
--- app/v1/sessions.js
+++ app/v1/sessions.js
@@ -24,17 +24,39 @@ module.exports =  function(express, version, passport){
     /////////////////////////
 
     // Handles login request
-    function login(req, res){
-        //Remove the unneeded fields for the response
-        let user = User.clean(req.user);
-        res.jsend.success({user: user}) 
+    function login(req, res, next){
+        
+        passport.authenticate(JSON_LOGIN_STRATEGY_KEY, function(err, user, info){
+            if (err) { return next(err); }
+            // If the user name or password ins incorect password return user = false
+            if (!user) { 
+                return res.status(401).jsend.fail({message:"Incorrect username or password"});  
+            }
+            //Remove the unneeded fields for the response
+            let userCleaned = User.clean(user);
+            res.jsend.success({user: userCleaned}) 
+
+        })(req, res, next);
     }
 
     // Handles signup request
-    function signup(req, res){
-        //Remove the unneeded fields for the response
-        let user = User.clean(req.user);
-        res.jsend.success({user: user}) 
+    function signup(req, res, next){
+    
+        passport.authenticate(JSON_SINGUP_STRATEGY_KEY, function(err, user, info){
+             console.log(res.user);
+            if (err) { return next(err); }
+            if (!user) { return 
+                res.status(400);
+                res.jsend.fail({message:"Incorrect username or password"}); 
+            }
+            req.logIn(user, function(err) {
+                if (err) { return next(err); }
+                //Remove the unneeded fields for the response
+                let user = User.clean(req.user);
+                res.jsend.success({user: user}) 
+            });
+        })(req, res, next);
+
     }
 
     // Handles logout request
@@ -57,8 +79,10 @@ module.exports =  function(express, version, passport){
     //Routes
     /////////////////////////
 
-    version.use(SIGNUP_ROUTE, passport.authenticate(JSON_SINGUP_STRATEGY_KEY), express.Router().post(EMPTY_PATH, signup));
-    version.use(LOGIN_ROUTE, passport.authenticate(JSON_LOGIN_STRATEGY_KEY), express.Router().post(EMPTY_PATH, login));
+    version.use(SIGNUP_ROUTE, express.Router().post(EMPTY_PATH, signup));
+    // version.use(SIGNUP_ROUTE, passport.authenticate(JSON_SINGUP_STRATEGY_KEY), express.Router().post(EMPTY_PATH, signup));
+    version.use(LOGIN_ROUTE, express.Router().post(EMPTY_PATH, login));
+        // version.use(LOGIN_ROUTE, passport.authenticate(JSON_LOGIN_STRATEGY_KEY), express.Router().post(EMPTY_PATH, login));
     version.use(LOGOUT_ROUTE, express.Router().get(EMPTY_PATH, logout));
 
 };
