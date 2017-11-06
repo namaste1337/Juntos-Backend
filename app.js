bae9445a78a7a18ac81b6b805b69619ac102343b
CheCm19 <che.cm@mail.com>
2017-11-05 23:23:37 -0800

    -Improves comments -Removes unneded SESSION_SECRET_KEY variable

--- app.js
+++ app.js
@@ -2,8 +2,6 @@
 // Requires
 ///////////////////////// 
 
-//Packages
-
 const express  		= require("express");
 const mongoose 		= require("mongoose");
 const passport 		= require("passport");
@@ -14,7 +12,10 @@ const session      	= require("express-session");
 const jsend 		= require("jsend");
 const fileUpload 	= require('express-fileupload');
 
-// Modules
+/////////////////////////
+// Configs
+///////////////////////// 
+
 const configDB     	 = require("./config/database.js");
 const configSession  = require("./config/session.js"); 
 
@@ -22,16 +23,16 @@ const configSession  = require("./config/session.js");
 // Constants
 ///////////////////////// 
 
+// Strings
 const ETAGS_KEY							= "etag";
 const PASSPORT_CONFIG_PATH 				= "./config/passport";
 const MORGAN_FORMAT		   				= "dev";
-const SESSION_SECRET_KEY   				= "f239hd794298hfd8623b(&F#(HFFHq93rh98wefh23";
-//Routes
+//Versioning
 const VERSION_1_PATH					= "/api/v1";
 //Static Routes
 const USER_PROFILE_IMAGES_PATH 			= "/user_profile_images";
 const USER_PROFILE_IMAGES_VIRTUAL_PATH 	= "user_profile_images"
-//Controllers
+//Routes
 const SESSION_CONTROLLER_FILE_PATH 		= "./app/v1/sessions.js";
 const UPLOADS_CONTROLLER_FILE_PATH 		= "./app/v1/uploads.js";
 const PROJECTS_CONTROLLER_FILE_PATH     = "./app/v1/projects.js";
@@ -60,13 +61,18 @@ app.use(jsend.middleware); //Set up Jsend to standardize responses
 app.use(session({ 
 secret: configSession.SESSION_SECRET_KEY, // session secret
 cookie:{
-	maxAge: configSession.SESSION_COOKIE_MAX_AGE // Cookie expires in 90 days
+	maxAge: configSession.SESSION_COOKIE_MAX_AGE
 }
 })); 
 app.use(passport.initialize()); // Initialize passport
 app.use(passport.session()); // persistent login sessions
 app.use(fileUpload()) // Set up express-fileupload middleware
 app.disable(ETAGS_KEY); // Disabled etags
+
+/////////////////////////
+// Static Paths 
+///////////////////////// 
+
 // Configure user profile images static path
 app.use(USER_PROFILE_IMAGES_PATH, express.static(USER_PROFILE_IMAGES_VIRTUAL_PATH)) 
 
@@ -78,9 +84,10 @@ app.use(USER_PROFILE_IMAGES_PATH, express.static(USER_PROFILE_IMAGES_VIRTUAL_PAT
 let v1 = express.Router();
 // Define version 
 app.use(VERSION_1_PATH, v1);
-// Version 1 API"s
-require(SESSION_CONTROLLER_FILE_PATH)(express, v1, passport); // load our routes and pass in our app and fully configured passport
+// Version 1 API"s - Load our routes and pass in our app and fully configured passport
+require(SESSION_CONTROLLER_FILE_PATH)(express, v1, passport); 
 require(UPLOADS_CONTROLLER_FILE_PATH)(express, v1, passport);
 require(PROJECTS_CONTROLLER_FILE_PATH)(express, v1, passport);
+
 // Export App as a module
 module.exports = app;
