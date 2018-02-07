2d6983c2f30359220fd43d48e2ecdc9a8dcd1964
CheCm19 <che.cm@mail.com>
2018-02-06 20:45:03 -0800

    Implements username requirement to user signup endpoint

--- app/v1/users.js
+++ app/v1/users.js
@@ -18,7 +18,8 @@ module.exports =  function(express, version, passport){
   const USERS_ROUTE                 = "/users";
   const EMPTY_PATH                  = "";
   // Error messges
-  const ERROR_USER_NAME_UNAVAILABLE   = "Username is unavailable";
+  const ERROR_EMAIL_UNAVAILABLE     = "Email is unavailable";
+  const ERROR_USERNAME_UNAVAILABLE  = "Username is unavailable"; 
 
   /////////////////////////
   // Request Handlers 
@@ -26,20 +27,49 @@ module.exports =  function(express, version, passport){
 
   // Handles signup request
   function signup(req, res, next){
-  
+
+    // A requirement for registration calls for capturing a username,
+    // but do to the limitation of passport only supporting email or user.
+    // In the following we will check if the username passed by the client
+    // exist in our users collection and return the approprite response.
+    // Once passport has handles the user creation we append the user name
+    // as the last operation of the sign up request.
+
+    let username = req.body.username;
+
+    User.findOne({"local.username" : username}, function(err, user){
+
+      if(user){
+         return res.status(errorCodes.ERROR_CODE_409).jsend.fail({message:ERROR_USERNAME_UNAVAILABLE}); 
+      }
+
       passport.authenticate(JSON_SINGUP_STRATEGY_KEY, function(err, user, info){
-          if (err) { return next(err); }
-          // If the username is unavailable user = false
-          if (!user) { 
-              return res.status(errorCodes.ERROR_CODE_409).jsend.fail({message:ERROR_USER_NAME_UNAVAILABLE}); 
-          }
-          req.logIn(user, function(err) {
-              if (err) { return next(err); }
+        if (err) { return next(err); }
+        // If the username is unavailable user = false
+        if (!user) { 
+            return res.status(errorCodes.ERROR_CODE_409).jsend.fail({message:ERROR_EMAIL_UNAVAILABLE}); 
+        }
+        req.logIn(user, function(err) {
+
+            if (err) { return next(err); }
+            
+            // If all is well lets append the username to the user object
+            user.local.username = username;
+
+            user.save(function(err){
+
+              if(err)
+                throw err;
+
               //Remove the unneeded fields for the response
               let user = User.clean(req.user);
+              // Return the cleaned user object in the reponse body
               res.jsend.success({user: user}) 
-          });
+
+            })
+        });
       })(req, res, next);
+    })
 
   }
 
