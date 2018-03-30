ac83e728743535a6f38efeeb1b197033a707f1ab
CheCm19 <che.cm@mail.com>
2018-03-29 17:15:33 -0700

    Cleans up comments for app/common/authentication.js

--- app/common/authentication.js
+++ app/common/authentication.js
@@ -1,17 +1,21 @@
+/////////////////////////
+// Requires
+///////////////////////// 
+
 let errorCodes = require("./errorCodes");
 
 
 module.exports = {
 
-	// route middleware to make sure a user is logged in
+	// Route middleware to make sure the calling user is logged in.
 	isLoggedIn: function (req, res, next) {
 
-    	// if user is authenticated in the session, carry on 
+    	// If user is authenticated in the session, carry on.
     	if (req.isAuthenticated()){
     		return next();
     	}
    
-    	// If user is unauthorized return an error
+    	// If user is unauthorized return a 401 error.
     	res.status(errorCodes.ERROR_CODE_401).jsend.fail({message: "Unauthorized" });
  
 	}
